43fccc34cfc20e548c2c81a103a52087fedc65fa
ngasc <nathan.gasc@idakto.com>
2022-06-03 17:39:12 +0200

    fix pipline github release

--- ops/releaseGithub.ts
+++ ops/releaseGithub.ts
@@ -1,39 +1,15 @@
 import { Octokit } from "octokit";
-import yargs from "yargs";
 import fs from "fs"
+require('dotenv').config();
+
+const args:any = {}
+args.auth = process.env.AUTH
+args.owner = process.env.OWNER
+args.repo = process.env.REPO
+
+console.log("arguments:",args);
 
 var start = async ()=>{
-    const argv = await yargs.command('pipeline:release:github', 'Generate a github release', {
-        auth: {
-            description: 'your auth token for github',
-            alias: 'a',
-            type: 'string'
-        },
-        owner: {
-            description: 'the project owner',
-            alias: 'o',
-            type: 'string'
-        },
-        repo: {
-            description: 'the repo name',
-            alias: 'r',
-            type: 'string'
-        }
-    })
-    
-    const args = await argv.parse()
-    if(!args.a && !args.auth){
-        throw new Error("You need to pass your github's auth token with -a parameter");
-    }
-
-    if(!args.o){
-        throw new Error("You need to pass the repo owner account name with -o parameter");
-    }
-
-    if(!args.r){
-        throw new Error("You need to pass the repo name with -r parameter");
-    }
-
     const pkgFile = fs.readFileSync("./package.json","utf-8");
     const versionRaw = /"version": ".*"/.exec(pkgFile)!
     const nameRaw = /"name": ".*"/.exec(pkgFile)!
@@ -46,12 +22,12 @@ var start = async ()=>{
     // Octokit.js
     // https://github.com/octokit/core.js#readme
     const octokit = new Octokit({
-        auth: args.a || args.auth
+        auth: args.auth
     })
     
-    const res = octokit.request('POST /repos/'+args.o+'/'+args.r+'/releases', {
-        owner: args.o,
-        repo: args.r,
+    const res = octokit.request('POST /repos/'+args.owner+'/'+args.repo+'/releases', {
+        owner: args.owner,
+        repo: args.repo,
         tag_name: "v"+version,
     })
     
