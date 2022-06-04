6df53f4b9ad9027b1a6fef06489c5b9f2387cd66
Nathan GASC <nathangasc@yahoo.fr>
2022-06-04 21:02:31 +0200

    change release Github script for test

--- ops/releaseGithub.ts
+++ ops/releaseGithub.ts
@@ -2,15 +2,15 @@ import { Octokit } from "octokit";
 import fs from "fs"
 require('dotenv').config();
 
-const args:any = {}
+const args: any = {}
 args.auth = process.env.AUTH
 args.owner = process.env.OWNER
 args.repo = process.env.REPO
 
-console.log("arguments:",args);
+console.log("arguments:", args, process.env);
 
-var start = async ()=>{
-    const pkgFile = fs.readFileSync("./package.json","utf-8");
+var start = async () => {
+    const pkgFile = fs.readFileSync("./package.json", "utf-8");
     const versionRaw = /"version": ".*"/.exec(pkgFile)!
     const nameRaw = /"name": ".*"/.exec(pkgFile)!
 
@@ -18,19 +18,19 @@ var start = async ()=>{
     const name = nameRaw[0]!.split("\"")[3];
 
     console.log(`Generate release for ${name}:${version}`);
-    
+
     // Octokit.js
     // https://github.com/octokit/core.js#readme
     const octokit = new Octokit({
         auth: args.auth
     })
-    
-    const res = octokit.request('POST /repos/'+args.owner+'/'+args.repo+'/releases', {
+
+    const res = octokit.request('POST /repos/' + args.owner + '/' + args.repo + '/releases', {
         owner: args.owner,
         repo: args.repo,
-        tag_name: "v"+version,
+        tag_name: "v" + version,
     })
-    
+
     res.then((res) => {
         console.log("request status:" + res.status)
     })
