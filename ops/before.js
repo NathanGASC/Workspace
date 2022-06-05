fd3e34715fb2defe778220b2681393eac9c254d0
Nathan GASC <nathangasc@yahoo.fr>
2022-06-05 14:24:33 +0200

    Issue#1: delete server and client folder seperatly

--- ops/before.js
+++ ops/before.js
@@ -3,7 +3,8 @@ var copy = require('recursive-copy');
 const del = require('del');
 require('dotenv').config();
 
-del.sync([path.resolve(__dirname, "./../dist")]);
+del.sync([path.resolve(__dirname, "./../dist/client")]);
+del.sync([path.resolve(__dirname, "./../dist/server")]);
 
 copy(path.resolve(__dirname, "./../src/client/views"), path.resolve(__dirname, "./../dist/client/views"), function (error, results) { });
 copy(path.resolve(__dirname, "./../src/client/assets"), path.resolve(__dirname, "./../dist/client/assets"), function (error, results) { });
@@ -19,6 +20,6 @@ copy(path.resolve(__dirname, "./../src/client/index.css"), path.resolve(__dirnam
 copy(path.resolve(__dirname, "./../src/server/entities/ressources/index.html"), path.resolve(__dirname, "./../dist/client/ui/index.html"), function (error, results) { });
 copy(path.resolve(__dirname, "./../src/server/entities/ressources/index.css"), path.resolve(__dirname, "./../dist/client/ui/index.css"), function (error, results) { });
 
-if(process.env.COPY_SERVER == "TRUE"){
-    copy(path.resolve(__dirname, "./../src/server/"), path.resolve(__dirname, "./../dist/server/"), function(error, results) {});
+if (process.env.COPY_SERVER == "TRUE") {
+    copy(path.resolve(__dirname, "./../src/server/"), path.resolve(__dirname, "./../dist/server/"), function (error, results) { });
 }
